a7ccf343cc1a09e13e6f6d3635993557d228e14d
Paul Bowen-Huggett <paulhuggett@mac.com>
2025-11-18 20:25:38 +0000

    Add port

---
+++ keysom/core_portme.mak
@@ -0,0 +1,94 @@
+# The spike executable should be on the path.
+
+ifndef TOOLCHAIN_ROOT
+$(error "Set TOOLCHAIN_ROOT to the toolchain root directory (clang-keysom)")
+endif
+
+RVARCH:=rv32imc
+
+LIBS =                                                        \
+  $(TOOLCHAIN_ROOT)/lib/clang/21/lib/riscv32-keysom-none-elf/libclang_rt.builtins.a \
+  $(TOOLCHAIN_ROOT)/picolibc/rv32-keysom/lib/libc.a           \
+  $(TOOLCHAIN_ROOT)/picolibc/rv32-keysom/lib/crt0-semihost.o
+
+# Flag : OUTFLAG
+#	Use this flag to define how to to get an executable (e.g -o)
+OUTFLAG= -o
+# Flag : CC
+#	Use this flag to define compiler to use
+CC 		= $(TOOLCHAIN_ROOT)/bin/clang
+# Flag : LD
+#	Use this flag to define compiler to use
+LD		= $(CC)
+# Flag : AS
+#	Use this flag to define compiler to use
+AS		= $(CC)
+# Flag : CFLAGS
+#	Use this flag to define compiler options. Note, you can add compiler options from the command line using XCFLAGS="other flags"
+PORT_CFLAGS =      \
+  -march=$(RVARCH) \
+  -O3              \
+  -I $(TOOLCHAIN_ROOT)/picolibc/rv32-keysom/include
+
+FLAGS_STR = "$(PORT_CFLAGS) $(XCFLAGS) $(XLFLAGS) $(LFLAGS_END)"
+CFLAGS = $(PORT_CFLAGS) -I$(PORT_DIR) -I. -DFLAGS_STR=\"$(FLAGS_STR)\"
+#Flag : LFLAGS_END
+#	Define any libraries needed for linking or other flags that should come at the end of the link line (e.g. linker scripts).
+#	Note : On certain platforms, the default clock_gettime implementation is supported but requires linking of librt.
+LFLAGS_END =
+# Flag : SEPARATE_COMPILE
+SEPARATE_COMPILE=1
+
+# How we create an object file and link.
+OBJOUT 	= -o
+LFLAGS 	=                     \
+  -march=$(RVARCH)            \
+  -nostdlib                   \
+  -O3                         \
+  $(LIBS)                     \
+  -Wl,-T,$(PORT_DIR)/spike.ld \
+  -Wl,--Map=map.txt           \
+  -Wl,--gc-sections
+ASFLAGS = -c
+OFLAG 	= -o
+COUT 	= -c
+
+LFLAGS_END =
+# Flag : PORT_SRCS
+# 	Port specific source files can be added here
+#	You may also need cvt.c if the fcvt functions are not provided as intrinsics by your compiler!
+PORT_SRCS =                 \
+  $(PORT_DIR)/core_portme.c \
+  $(PORT_DIR)/spike_glue.c
+
+vpath %.c $(PORT_DIR)
+vpath %.s $(PORT_DIR)
+PORT_OBJS = $(PORT_SRCS:%.c=%.o)
+
+LOAD = echo Loading done
+
+# Use the simulator to run the executable. Spike is assumed to be on the path.
+RUN = spike --isa=$(RVARCH)_Zicntr -m0x80000000:0x1000000
+
+OEXT = .o
+EXE = .elf
+
+$(OPATH)$(PORT_DIR)/%$(OEXT) : %.c
+	$(CC) $(CFLAGS) $(XCFLAGS) $(COUT) $< $(OBJOUT) $@
+
+$(OPATH)%$(OEXT) : %.c
+	$(CC) $(CFLAGS) $(XCFLAGS) $(COUT) $< $(OBJOUT) $@
+
+$(OPATH)$(PORT_DIR)/%$(OEXT) : %.s
+	$(AS) $(ASFLAGS) $< $(OBJOUT) $@
+
+# Target : port_pre% and port_post%
+# For the purpose of this simple port, no pre or post steps needed.
+
+.PHONY : port_prebuild port_postbuild port_prerun port_postrun port_preload port_postload
+port_pre% port_post% :
+
+# FLAG : OPATH
+# Path to the output folder. Default - current folder.
+OPATH = ./
+MKDIR = mkdir -p
---
+++ keysom/spike_glue.c
@@ -0,0 +1,359 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+enum {
+  sys_exit = 93,
+  sys_read = 63,
+  sys_write = 64,
+  sys_close = 57,
+  sys_lseek = 62,
+  sys_openat = 56,
+  sys_getmainvars = 2011,
+};
+
+enum { channel_stdin = 0, channel_stdout = 1, channel_stderr = 2 };
+
+extern volatile uint64_t tohost;
+volatile uint64_t tohost;
+extern volatile uint64_t fromhost;
+volatile uint64_t fromhost;
+
+__attribute__((noinline)) static uint64_t syscall(uint64_t n, uint64_t a0,
+                                                  uint64_t a1, uint64_t a2,
+                                                  uint64_t a3, uint64_t a4,
+                                                  uint64_t a5, uint64_t a6) {
+  uint64_t const th = tohost;
+  if (th) {
+    __builtin_trap();
+  }
+
+  static volatile uint64_t htif_mem[8];
+  htif_mem[0] = n;
+  htif_mem[1] = a0;
+  htif_mem[2] = a1;
+  htif_mem[3] = a2;
+  htif_mem[4] = a3;
+  htif_mem[5] = a4;
+  htif_mem[6] = a5;
+  htif_mem[7] = a6;
+  tohost = (uint64_t)htif_mem;
+
+  for (;;) {
+    uint64_t const fh = fromhost;
+    if (fh) {
+      fromhost = 0;
+      break;
+    }
+  }
+  return htif_mem[0];
+}
+
+
+
+// A small buffer for stdout. Flushed when full or '\n' is written to the device.
+static char buffer[256];
+static size_t pos = 0;
+
+static int stdout_flush(FILE *f) {
+  (void)f;
+  if (pos > 0) {
+    syscall(sys_write, channel_stdout, (uintptr_t)buffer, pos, 0, 0, 0, 0);
+    pos = 0;
+  }
+  return 0;
+}
+
+static int stdout_writec(char c, FILE *file) {
+  buffer[pos++] = c;
+  if (pos >= sizeof(buffer) / sizeof(buffer[0]) || c == '\n') {
+    stdout_flush(file);
+  }
+  return c;
+}
+
+static void stdout_write_str(char const * str) {
+  for (; *str != '\0'; ++str) {
+    stdout_writec(*str, NULL);
+  }
+}
+
+
+// A callback for picolibc stdio providing unbuffered output for stderr.
+static int stderr_writec(char c, FILE *f) {
+  (void)f;
+  syscall(sys_write, channel_stderr, (uintptr_t)&c, 1, 0, 0, 0, 0);
+  return c;
+}
+
+// putc, get, flush for stdin, stdout, and stderr.
+#define FDEV_SETUP_WRITE __SWR
+static FILE __stdio = FDEV_SETUP_STREAM(stdout_writec, NULL, stdout_flush, FDEV_SETUP_WRITE);
+FILE *const stdout = &__stdio;
+__strong_reference(stdout, stdin);
+
+static FILE __stdio_stderr = FDEV_SETUP_STREAM(stderr_writec, NULL, NULL, FDEV_SETUP_WRITE);
+FILE *const stderr = &__stdio_stderr;
+
+[[noreturn]] void _exit(int const status) {
+  stdout_flush(stdout);
+  syscall(sys_exit, (uint64_t)status, 0, 0, 0, 0, 0, 0);
+  for (;;) {
+  }
+  __builtin_unreachable();
+}
+
+int sys_semihost_get_cmdline(char *buf, size_t size) {
+  uint64_t args[64];
+  uint64_t const res = syscall(sys_getmainvars, (uintptr_t)args, sizeof(args), 0, 0, 0, 0, 0);
+  if (res != 0) {
+    // args buffer overflowed. Return an error to picolibc.
+    return -1;
+  }
+
+  // Turn the array of separate args that was returned by spike into a single
+  // space-separated array for picolibc.
+  char *orig_buf = buf;
+  uint64_t argc = args[0];
+  for (unsigned ctr = 0; ctr < argc; ++ctr) {
+    if (ctr > 0) {
+      *(buf++) = ' ';
+      --size;
+    }
+
+    char const *argv = (char const *)args[ctr + 1];
+    size_t const len = strlen(argv);
+    if (len >= size) {
+      return -1;
+    }
+    memcpy(buf, argv, len);
+    buf += len;
+    size -= len;
+    if (size == 0) {
+      // No space left. Return an error to picolibc.
+      return -1;
+    }
+  }
+  *buf = '\0';
+  return 0; // success
+}
+
+
+
+typedef struct file {
+  int kfd; // file descriptor on the host side of the HTIF
+  unsigned refcnt;
+} file_t;
+
+#define MAX_FDS 128
+static file_t* fds[MAX_FDS];
+#define MAX_FILES 128
+file_t files[MAX_FILES] = {[0 ... MAX_FILES-1] = {-1,0}};
+
+static void file_incref(file_t * const f) {
+  ++f->refcnt;
+  //kassert(prev > 0);
+}
+
+static void file_decref(file_t* f) {
+  if (f->refcnt-- == 2) {
+    f->refcnt = 0;
+    syscall(sys_close, f->kfd, 0, 0, 0, 0, 0, 0);
+  }
+}
+
+static file_t* file_get_free() {
+  for (file_t* f = files; f < files + MAX_FILES; f++) {
+    if (f->refcnt == 0) {
+      f->refcnt = 2;
+      return f;
+    }
+  }
+  return NULL;
+}
+
+// Given a file_t, returns a new file descriptor.
+int file_dup(file_t* f) {
+  for (int i = 3; i < MAX_FDS; i++) {
+    if (fds[i] == NULL) {
+      fds[i] = f;
+      file_incref(f);
+      return i;
+    }
+  }
+  return -1;
+}
+static file_t* file_get(int fd) {
+  if (fd < 0 || fd >= MAX_FDS) {
+    return NULL;
+  }
+  file_t * const f = fds[fd];
+  if (f->refcnt == 0) {
+    return NULL;
+  }
+  ++f->refcnt;
+  return f;
+}
+
+
+#undef AT_FDCWD
+#define AT_FDCWD -100
+
+static int at_kfd(int dirfd) {
+  if (dirfd == AT_FDCWD) {
+    return AT_FDCWD;
+  }
+  file_t* const dir = file_get(dirfd);
+  if (dir == NULL) {
+    return -1;
+  }
+  int kfd = dir->kfd;
+  file_decref(dir);
+  return kfd;
+}
+
+
+
+#define IS_ERR_VALUE(x) ((unsigned long)(x) >= (unsigned long)-4096)
+#define ERR_PTR(x) ((void*)(long)(x))
+#define PTR_ERR(x) ((long)(x))
+
+file_t* file_openat(int dirfd, char const * fn, int flags, int mode) {
+  file_t * const f = file_get_free();
+  if (f == NULL) {
+    return ERR_PTR(-ENOMEM);
+  }
+
+  size_t fn_size = strlen(fn) + 1;
+  long ret = syscall(sys_openat, dirfd, (uint64_t)(uintptr_t)fn, fn_size, flags, mode, 0, 0);
+  if (ret >= 0) {
+    f->kfd = ret;
+    return f;
+  }
+  return ERR_PTR(ret);
+}
+
+int open(char const* name, int flags, ...) {
+  int kfd = at_kfd(AT_FDCWD);
+  if (kfd == -1) {
+    return -EBADF;
+  }
+
+  int mode = 0;
+  if (flags & O_CREAT) {
+    va_list args;
+    va_start(args, flags);
+    mode = va_arg(args, int);
+    va_end(args);
+  }
+
+  file_t* file = file_openat(kfd, name, flags, mode);
+  if (IS_ERR_VALUE(file)) {
+    return PTR_ERR(file);
+  }
+
+  int fd = file_dup(file);
+  file_decref(file); // counteract file_dup's file_incref
+  if (fd < 0) {
+    return -ENOMEM;
+  }
+  return fd;
+}
+
+ssize_t read(int fd, void*buf, size_t nbyte){
+  file_t * const f = file_get(fd);
+  if (f == NULL) {
+    return -1;
+  }
+
+  int res = syscall(sys_read, f->kfd, (uint64_t)(uintptr_t)buf, nbyte, 0, 0, 0, 0);
+  if (res < 0) {
+    errno = res;
+    res = -1;
+  }
+  return res;
+}
+ssize_t write(int fd, void const*buf, size_t nbyte) {
+  file_t * const f = file_get(fd);
+  if (f == NULL) {
+    return -1;
+  }
+  return syscall(sys_write, f->kfd, (uint64_t)(uintptr_t)buf, nbyte, 0, 0, 0, 0);
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+  file_t * const f = file_get(fd);
+  if (f == NULL) {
+    return -1;
+  }
+  int res = syscall(sys_lseek, f->kfd, offset, whence, 0, 0, 0, 0);
+  if (res < 0) {
+    errno = res;
+    res = -1;
+  }
+  return res;
+}
+
+int close(int fd) {
+  file_t * const f = file_get(fd);
+  if (f == NULL) {
+    return -1;
+  }
+  return syscall(sys_close, f->kfd, 0, 0, 0, 0, 0, 0);
+}
+
+// Read the RISC-V time CSR (cycle counter)
+static inline uint64_t read_time(void) {
+  // For RV32, need to read timeh and time with proper synchronization
+  uint32_t hi = 0;
+  uint32_t lo = 0;
+  uint32_t tmp = 0;
+  do {
+    __asm__ volatile("rdtimeh %0" : "=r"(hi));
+    __asm__ volatile("rdtime %0" : "=r"(lo));
+    __asm__ volatile("rdtimeh %0" : "=r"(tmp));
+  } while (hi != tmp);
+  return ((uint64_t)hi << 32) | lo;
+}
+
+static uint64_t sys_semihost_elapsed(void) {
+  return read_time();
+}
+static uintptr_t sys_semihost_tickfreq() {
+  return 5000000ULL;
+}
+uintptr_t sys_semihost_time(void) {
+  return 0;
+}
+
+int gettimeofday(struct timeval *restrict tv, void *restrict tz) {
+  uint64_t ticks;
+  static uint64_t start_elapsed;
+  static uint64_t start_ticks;
+  static uintptr_t tick_freq;
+  static _Bool been_here = false;
+
+  (void) tz;
+  uint64_t const elapsed = read_time();
+  if (!been_here) {
+    start_elapsed = elapsed;
+    tick_freq = sys_semihost_tickfreq();
+    start_ticks = 0;
+    been_here = 1;
+  }
+
+  ticks = start_ticks + (elapsed - start_elapsed);
+
+  tv->tv_sec = (time_t) (ticks / tick_freq);
+  tv->tv_usec = (suseconds_t) ((ticks % tick_freq) * 1000000 / tick_freq);
+  return 0;
+}
